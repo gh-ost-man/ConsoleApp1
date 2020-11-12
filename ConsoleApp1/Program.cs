84e45011b4fdaff826640e9b9841951a35c3930e
lilyakaras <lilyca.hrupa@gmail.com>
2020-11-12 11:45:31 +0200

    MarksCommit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CreditCardLib;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Aloha");
+            CreditCard creditCard = new CreditCard();
         }
     }
 }
---
+++ CreditCardLib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardLib
+{
+    public class CreditCard
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
